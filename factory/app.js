e2231c3ce7f7455b54f59f51610e0d85bc4ac2b5
Elias Moreira <elias_moreirajunior@mastercard.com>
2016-03-04 23:18:45 +0000

    function name changed

--- factory/app.js
+++ factory/app.js
@@ -8,33 +8,33 @@
 
 			vm.view = {};
 
+			//simples functions to change the state	
 		    vm.case1 = function() {
 
-		        vm.view = getView('case1');
+		        vm.view = getFactoryView('case1');
 		    }
 
 		    vm.case2 = function() {
-		         vm.view = getView('case2');
+		         vm.view = getFactoryView('case2');
 		    }
 
 		    vm.case3 = function() {
-		         vm.view = getView('case3');
+		         vm.view = getFactoryView('case3');
 		    }
 
-		    function getView(viewCase) {
+		    function getFactoryView(viewCase) {
 
-		    	console.log("CASE", viewCase)		
-
+		    	//Default view, components that are common between the views.	
 		        var defaultView = {
 		            title : 'Default Title',
 		            templateUrl : 'templateDefault.html',
 		            showFirstBlock : true
 		        }
 
-		        //LEVEL 1
+		        //CASE VIEW 1, it creates case1 inherits the atributes/methos from defaultView
 		        vm.view.case1 = Object.create(defaultView);
 
-		        //LEVEL 2
+		        //CASE VIEW 2 it creates case2 inherits the atributes/methos from defaultView
 		        vm.view.case2 = Object.create(defaultView);
 		        vm.view.case2.showFirstBlock = false;
 		        vm.view.case2.showList = true;
@@ -45,20 +45,23 @@
 		            console.log("function of case 2")
 		        }
 
+				//CASE VIEW 2
 		        vm.view.case3 = Object.create(vm.view.case2)
 		        vm.view.case3.templateUrl = 'template-case3.html?cache='+ new Date();
 		        vm.view.case3.title = "Case 3";
 
+				//it creates case3 inherits the atributes/methos from defaultView and ALSO Case 2
 		        vm.view.case3.callCase3 = function() {
 		            console.log("function of case 3!!")
 		        }
-		       
+		       	
+		       	//Return the ideal view 
 		        return vm.view[viewCase];
 		    }
 
 		    function init() {
 
-				vm.view = getView('case1')
+				vm.view = getFactoryView('case1')
 
 		    }
 
